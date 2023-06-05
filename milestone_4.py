a32d965fee479722b76d26a9d4f507df6b81d6b4
prg48 <32218486+prg48@users.noreply.github.com>
2023-06-05 19:09:20 +0100

    create methods for running the checks

--- milestone_4.py
+++ milestone_4.py
@@ -28,3 +28,28 @@ class HangMan:
         self.word_list = word_list
         self.num_lives = num_lives
         self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        """
+        This method checks if the guess is in the word
+
+        Args:
+            guess (char): a letter guess.
+        """
+        guess = guess.lower()
+        if guess in self.word:
+            print("Good guess! {guess} is in the word.")
+    
+    def ask_for_input(self):
+        """
+        This method asks for input from the player and checks guesses.
+        """
+        while True:
+            guess = input("Please guess a letter: ")
+            if len(guess) > 1 and not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetic character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)

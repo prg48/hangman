b4ac01eb3742f5db9c143a2ef089481ae0f34d68
prg48 <32218486+prg48@users.noreply.github.com>
2023-06-05 15:59:02 +0100

    check whether the guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,16 @@
+
+import random
+
+word_list = ["apple", "banana", "pears", "grapes", "watermelon"]
+word = random.choice(word_list)
 while True:
     guess = input("Please guess a letter: ")
     if len(guess) == 1 and guess.isalpha():
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
